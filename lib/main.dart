23a34577903e2f39cf98e97fe569e66765252c2b
Hira-kiran <hirakiran224@gmail.com>
2022-12-17 01:37:16 -0800

    name

--- lib/main.dart
+++ lib/main.dart
@@ -28,11 +28,9 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Notifications',
       home: Notifications(),
     );
